9cded2d62cadab151d98089cf83a6dc85a0da933
keen91 <kinan.hatahet@gmail.com>
2022-05-09 15:39:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+// const body = document.getElementsByTagName('body');
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Kinan is the champion';
+newHeader.setAttribute('id', 'victory');
+document.body.append(newHeader);
